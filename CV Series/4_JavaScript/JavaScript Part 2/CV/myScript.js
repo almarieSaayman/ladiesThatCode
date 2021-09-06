55c43770873cb4b358a13578be2a960fe2f72b9e
Zakiya Safi <zakiyas@discovery.co.za>
2021-09-06 08:24:28 +0200

    JS part 2 slides + demo

---
+++ CV Series/4_JavaScript/JavaScript Part 2/CV/myScript.js
@@ -0,0 +1,20 @@
+
+function navigateToHome() {
+    console.log('navigating to home')
+}
+
+function navigateToResume() {
+    console.log('navigating to resume')
+}
+
+function navigateToContact() {
+    console.log('navigating to contact')
+}
+
+function contactMeAlert() {
+    alert('You tryin to contact me?! Send me an email!!')
+}
+
+function showMySkills() {
+    document.getElementById("aboutme").innerHTML  = "Yeah I gotz lots of skills! JavaScript. JavaScript. and umm.. more JavaScript?!";
+}
