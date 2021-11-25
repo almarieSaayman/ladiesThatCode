08bb8aef6d709fe99f3a2b517d9439a661bad6e5
Tsholofelo <tsholofelo.pole@entelect.co.za>
2021-11-25 08:25:30 +0200

    Added slides and code to publish and deploy our CV

---
+++ CV Series/5_Package and deploy CV/main.js
@@ -0,0 +1,23 @@
+ 
+const {app, BrowserWindow} = require('electron')
+  const path = require('path')
+  const url = require('url')
+  
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600})
+  
+   // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'cv.html'),
+      protocol: 'file:',
+      slashes: true,
+	  show: false,
+	  //backgroundColor: '#ffff00'
+    }))
+	
+	win.once('ready-to-show', win.show)
+	win.removeMenu()
+  }
+  
+  app.on('ready', createWindow)
